f7beff1527162865b9dfe1320b4aab5b24ad3941
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-28 17:26:39 -0500

    first object

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+let student = {
+    name: "Bill",
+    age: 25,
+    isMarried: false,
+    grades: [90, 85, 75, 95, 90],
+    address: {
+        street: "123 Main St",
+        city: "Los Angeles",
+        state: "California",
+        zip: 90001
+    }
+};
+
+console.log(student);
